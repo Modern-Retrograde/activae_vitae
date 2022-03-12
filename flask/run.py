a587af6573535342fbf26eb8062e484e4817bc96
engeftroll <engeftroll@gmail.com>
2022-03-12 12:52:58 +0300

    Подготовка боевой единицы...
    1. Создан Procfile: говорит, как включать сервер.
    2. Перемещён файл requirements.txt, чтобы Heroku его не терял из виду...
    3. Дополнен run.py: хост и порт теперь выбирается из консоли.

--- flask/run.py
+++ flask/run.py
@@ -1,6 +1,7 @@
 # Здесь запускается Flask.
 
 if __name__ == '__main__':
+    from sys import argv
     from configs import flask_debug
     from main import app as working_app
     from configs import flask_csrf_enabled, flask_secret_key
@@ -8,4 +9,7 @@ if __name__ == '__main__':
     working_app.config["CSRF_ENABLED"] = flask_csrf_enabled
     working_app.config["SECRET_KEY"] = flask_secret_key
 
-    working_app.run(debug=flask_debug)
+    host = argv[1]
+    port = int(argv[2])
+
+    working_app.run(host=host, port=port, debug=flask_debug)
